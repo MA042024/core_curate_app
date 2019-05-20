1d9d2e2674b5ffdf30843505c2dac7b6c3e27110
Xavier SCHMITT <xavier.schmitt@nist.gov>
2019-05-20 14:47:58 -0400

    chore: upgrade 1.3.0.dev1
    Change-Id: Ifb0d7dac5afcb59e999770410e11566fb8e2bef2

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ required = [req_link(r) if r.startswith("https://") else r for r in required]
 
 setup(
     name="core_curate_app",
-    version="1.2.0",
+    version="1.3.0.dev1",
     description="Curation functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
