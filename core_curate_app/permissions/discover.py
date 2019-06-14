171d8aac0bb8f9a4d0f72c97203fd9caac7b5a76
Philippe Dessauw <philippe.dessauw@nist.gov>
2019-06-14 18:06:12 -0400

    fix: remove print + cleanup code
    Change-Id: I2970d1b9d38e9da045384fb988bf806fd581529d

--- core_curate_app/permissions/discover.py
+++ core_curate_app/permissions/discover.py
@@ -1,16 +1,16 @@
-""" Initialize permissions for core curate app
+""" Initialize permissions for core curate app.
 """
-from __future__ import print_function
+import logging
+
 from django.contrib.auth.models import Group, Permission
 import core_main_app.permissions.rights as main_rights
 import core_curate_app.permissions.rights as curate_rights
 
+logger = logging.getLogger(__name__)
+
 
 def init_permissions():
     """ Initialization of groups and permissions.
-
-    Returns:
-
     """
     try:
         # Get or Create the default group
@@ -18,11 +18,13 @@ def init_permissions():
 
         # Get curate permissions
         curate_access_perm = Permission.objects.get(codename=curate_rights.curate_access)
-        curate_view_data_save_repo_perm = Permission.objects.get(codename=curate_rights.curate_view_data_save_repo)
+        curate_view_data_save_repo_perm = Permission.objects.get(
+            codename=curate_rights.curate_view_data_save_repo
+        )
 
         # Add permissions to default group
         default_group.permissions.add(curate_access_perm,
                                       curate_view_data_save_repo_perm)
 
     except Exception as e:
-        print('ERROR : Impossible to init the permissions : ' + str(e))
+        logger.error("Impossible to init curate permissions: %s" % str(e))
