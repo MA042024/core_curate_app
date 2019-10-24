4500270cb6d5ee0985ccef259e3992181407f203
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-10-24 14:49:54 -0400

    fix: revert django upgrade
    Change-Id: Ib1b0ac58107729235b436e1f3ee20984802214fb

--- core_curate_app/admin.py
+++ core_curate_app/admin.py
@@ -1,14 +1,14 @@
 """
 Url router for the administration site
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.admin.views.decorators import staff_member_required
-from django.urls import re_path
 
 from core_curate_app.views.common import views as common_views
 
 admin_urls = [
-    re_path(r'^view-form/(?P<curate_data_structure_id>\w+)$', staff_member_required(common_views.FormView.as_view(
+    url(r'^view-form/(?P<curate_data_structure_id>\w+)$', staff_member_required(common_views.FormView.as_view(
         administration=True,
         template='core_curate_app/admin/view_curate_data_structure.html')),
         name='core_curate_view_form')
--- core_curate_app/menus.py
+++ core_curate_app/menus.py
@@ -1,7 +1,7 @@
 """ Add Curate in main menu
 """
 
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 from menu import Menu, MenuItem
 
 from core_curate_app.settings import CURATE_MENU_NAME
