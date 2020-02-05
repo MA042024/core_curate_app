31001a0ac4e8843509e83196f53ea9dafa9312d5
Augustin CHINI <augustin.chini@nist.gov>
2020-02-05 10:06:36 -0500

    chore: upgrade 1.8.0
    Change-Id: I4310a9d764fe7b7742757502b8a8cb21adde125a

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ required = [req_link(r) if r.startswith("https://") else r for r in required]
 
 setup(
     name="core_curate_app",
-    version="1.7.0",
+    version="1.8.0",
     description="Curation functionalities for the curator core project",
     long_description=long_desc,
     author="NIST IT Lab",
