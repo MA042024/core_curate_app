a7500e9a886bfed4c46e834fe886823a8f818eba
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2020-09-01 08:40:38 -0400

    docs: add settings doc
    Change-Id: I4104104532e72ecd3a1ffa4433e5719a842be8df

--- core_curate_app/settings.py
+++ core_curate_app/settings.py
@@ -1,4 +1,7 @@
 """Core Curate App Settings
+
+Settings with the following syntax can be overwritten at the project level:
+SETTING_NAME = getattr(settings, "SETTING_NAME", "Default Value")
 """
 import os
 
